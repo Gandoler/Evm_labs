fc03ff86e8040eeafcb83c47280903ef90d34a9a
Gleb <81429241+Gandoler@users.noreply.github.com>
2024-04-03 20:45:29 +0300

    Create num1.asm

---
+++ lab_5/num1.asm
@@ -0,0 +1,62 @@
+.data
+format_int16: .string "16-bit integer: %d\n"
+format_int32: .string "32-bit integer: %d\n"
+format_int64: .string "64-bit integer: %lld\n"
+format_float32: .string "32-bit float: %f\n"
+format_float64: .string "64-bit float: %f\n"
+myFlotik:  .float 3.14
+myDoublik: .float 3.14159265359
+
+.text
+.globl main
+main: 
+    sub     $40, %rsp
+    
+    movw    $6535, 2(%rsp)
+    movl    $23, 6(%rsp)
+    movq    $23234234, 14(%rsp)
+   
+    movss   myFlotik(%rip), %xmm0
+    movss   %xmm0, 22(%rsp)
+    
+    
+    
+    movss   myDoublik(%rip), %xmm1
+    movss   %xmm1, 32(%rsp)
+    
+    mov     2(%rsp), %eax
+    mov     %eax, %esi
+    lea     format_int16(%rip), %rdi  
+    xor     %eax, %eax
+    call    printf
+    
+    mov     6(%rsp), %eax
+    mov     %eax, %esi
+    lea     format_int32(%rip), %rdi  
+    xor     %eax, %eax
+    call    printf
+    
+    mov     14(%rsp), %rax
+    mov     %rax, %rsi
+    lea     format_int64(%rip), %rdi  
+    xor     %eax, %eax
+    call    printf
+    
+    cvtss2sd    22(%rsp), %xmm0
+ 
+    lea     format_float32(%rip), %rdi 
+    xor     $1, %eax
+    call    printf
+    xor     %eax, %eax
+    
+   
+    cvtss2sd    32(%rsp), %xmm0
+
+    lea     format_float64(%rip), %rdi 
+    xor     $1, %eax
+    call    printf
+    
+    
+    add     $40, %rsp
+    xor     %eax, %eax
+    ret
